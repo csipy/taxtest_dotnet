eb1dc2f540c47dac85c0ceed06f6ce254c68b1a1
AndyJohnson <csilla.csipak@yahoo.com>
2024-04-03 20:11:47 +0200

    initial commit

---
+++ TaxuallyTest/Global.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+
+namespace TaxuallyTest
+{
+    public static class Global
+    { public static async Task<IPage> SetupAndLoginAsync()
+        {
+            var playwright = await Playwright.CreateAsync();
+            var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+            {
+                Headless = false,
+            });
+
+            var context = await browser.NewContextAsync();
+            var page = await context.NewPageAsync();
+
+            await page.GotoAsync("https://github.com/login");
+            // Interact with login form
+            await page.GetByLabel("Username or email address").FillAsync("username");
+            await page.GetByLabel("Password").FillAsync("password");
+            await page.GetByRole(AriaRole.Button, new() { Name = "Sign in" }).ClickAsync();
+
+            await context.StorageStateAsync(new()
+            {
+                Path = "State\\storage.json"
+            });
+            return page;
+
+         }
+    }
+}
---
+++ TaxuallyTest/Login.cs
@@ -0,0 +1,50 @@
+﻿using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using NUnit.Framework;
+
+namespace PlaywrightTests;
+
+[Parallelizable(ParallelScope.Self)]
+[TestFixture]
+public class Tests : PageTest
+{
+    [Test]
+    public async Task HomepageHasPlaywrightInTitleAndGetStartedLinkLinkingtoTheIntroPage()
+    {
+            using var playwright = await Playwright.CreateAsync();
+            var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+            {
+                Headless = false,
+            });
+
+        var context = await browser.NewContextAsync(new()
+        {
+            StorageStatePath = "State\\storage.json"
+        });
+
+        //await context.StorageStateAsync(new()
+        //    {
+        //        Path = "State\\storage.json"
+        //    });
+
+            var page = await context.NewPageAsync();
+            // Expect a title "to contain" a substring.
+            await Expect(Page).ToHaveTitleAsync(new Regex("Playwright"));
+
+        // create a locator
+        var getStarted = Page.GetByRole(AriaRole.Link, new() { Name = "Get started" });
+
+        // Expect an attribute "to be strictly equal" to the value.
+        await Expect(getStarted).ToHaveAttributeAsync("href", "/docs/intro");
+
+        // Click the get started link.
+        await getStarted.ClickAsync();
+
+        // Expects page to have a heading with the name of Installation.
+        await Expect(Page
+            .GetByRole(AriaRole.Heading, new() { Name = "Installation" }))
+            .ToBeVisibleAsync();
+    }
+}
